baf7527f2b8aab3c21207b3c9da6b001edc6d590
manish kumar gupta <163879979+MannishGuptta@users.noreply.github.com>
2026-03-05 15:04:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-import MeetingLogger from "./MeetingLogger";
-import OnboardCP from "./OnboardCP";
-import MyCPs from "./MyCPs";
-import RecordSale from "./RecordSale";
+import LogMeeting from "./pages/LogMeeting";
 
 function Dashboard() {
   return (
-
     <div style={{ padding: 40 }}>
 
       <h1>RevenuePilot</h1>
       <h2>Sales Command System</h2>
 
       <div style={{ marginTop: 40 }}>
-
-        <Link to="/onboard-cp">
-          <button style={btn}>Onboard Channel Partner</button>
-        </Link>
-
-        <br /><br />
-
         <Link to="/log-meeting">
-          <button style={btn}>Log Meeting</button>
+          <button style={btn}>
+            Log Meeting
+          </button>
         </Link>
-
-        <br /><br />
-
-        <Link to="/my-cps">
-          <button style={btn}>My Channel Partners</button>
-        </Link>
-
-        <br /><br />
-
-        <Link to="/record-sale">
-          <button style={btn}>Record Sale</button>
-        </Link>
-
       </div>
 
     </div>
-
   );
 }
 
@@ -54,13 +30,7 @@ export default function App() {
 
         <Route path="/" element={<Dashboard />} />
 
-        <Route path="/onboard-cp" element={<OnboardCP />} />
-
-        <Route path="/log-meeting" element={<MeetingLogger />} />
-
-        <Route path="/my-cps" element={<MyCPs />} />
-
-        <Route path="/record-sale" element={<RecordSale />} />
+        <Route path="/log-meeting" element={<LogMeeting />} />
 
       </Routes>
 
@@ -75,6 +45,5 @@ const btn = {
   color: "white",
   border: "none",
   borderRadius: 6,
-  cursor: "pointer",
-  width: 250
+  cursor: "pointer"
 };
