6ba8cde9d3ebce5922cad69a6eb361b939e690c9
manish kumar gupta <163879979+MannishGuptta@users.noreply.github.com>
2026-03-06 12:13:47 +0530

    Update MeetingLogger.jsx

--- src/pages/MeetingLogger.jsx
+++ src/pages/MeetingLogger.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { supabase } from "../supabaseClient";
+
+export default function MeetingLogger() {
 const [channelPartners, setChannelPartners] = useState([]);
 const [selectedCP, setSelectedCP] = useState("");
-export default function MeetingLogger() {
-
   const [meetingWith, setMeetingWith] = useState("Channel Partner");
   const [name, setName] = useState("");
   const [meetingMode, setMeetingMode] = useState("Physical");
